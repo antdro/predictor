6e3304c30fe22169c581fa4dacb4e80a0355c6e8
Anton Drozdov <anton_drozdov@yahoo.co.uk>
2017-05-16 23:33:27 +0100

    add: scale_stats_per_minute() to data_processor.py

--- data_processor.py
+++ data_processor.py
@@ -14,3 +14,20 @@ def convert_kickoff_to_date(df):
     df.kickoff = [from_str_to_date(date) for date in list(df.kickoff)]
     
     return df
+
+
+
+def scale_stats_per_minute(df):
+    
+    """
+    Scale features in ['CLR','FC','FK','FS','G','GA','GC','GK','PAS','R','S','SAV','TA','Y'] per minute played
+    Returns updated df
+    """
+    
+    columns_to_scale = ['CLR', 'FC', 'FK', 'FS', 'G', 'GA', 'GC', 
+                    'GK', 'PAS', 'R', 'S', 'SAV', 'TA', 'Y']
+
+    for column in columns_to_scale:
+        df[column] = df[column] / df.MINS
+        
+    return df
