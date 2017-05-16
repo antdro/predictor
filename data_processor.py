3f235b16f4970bfa891387fea00720183d3adbd8
Anton Drozdov <anton_drozdov@yahoo.co.uk>
2017-05-16 23:45:27 +0100

    add: lineup_by_date() to data_processor.py
    returns team's lineup for a given date as a dictionary

--- data_processor.py
+++ data_processor.py
@@ -31,3 +31,32 @@ def scale_stats_per_minute(df):
         df[column] = df[column] / df.MINS
         
     return df
+
+
+
+def lineup_by_date(df, date, team):
+    
+    """
+    Returns team's lineup for a given date as a dictionary.
+    """
+    
+    
+    df = df[df.kickoff == date]
+    df = df[df.team == team]
+    
+    forwards = df[df.position == "forward"].player.tolist()
+    defenders = df[df.position == "defender"].player.tolist()
+    goalkeepers = df[df.position == "goalkeeper"].player.tolist()
+    midfielders = df[df.position == "midfielder"].player.tolist()    
+
+    lineup = {
+
+        "team" : team,
+        "date" : date,
+        "for" : forwards,
+        "mid" : midfielders,
+        "def" : defenders,
+        "goal" : goalkeepers
+    }
+    
+    return lineup
